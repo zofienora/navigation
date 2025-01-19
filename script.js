64eef12e97318cf0dc9cb3f98c5bb0ab4fde745b
Zofia Nora <sophienora92@gmail.com>
2025-01-19 12:17:02 +0100

    finished the design and buttons, function is working

---
+++ script.js
@@ -0,0 +1,9 @@
+const navbar = document.getElementById('navbar-right');
+
+function openSidebar() {
+    navbar.classList.add('show');
+}
+
+function closeSidebar() {
+    navbar.classList.remove('show');
+}
